5e38ac79db3a4bbc8137dfa04cd3bc5f134075a4
willshanego <wgoodrow@yahoo.com>
2017-03-14 18:42:01 -0500

    develop1

--- source/app.js
+++ source/app.js
@@ -4,34 +4,32 @@ require([], function(){
 		{
         Detector.addGetWebGLMessage();
         throw 'WebGL Not Available'
-		}
+    }
 
     var camera, scene, renderer;
 	var geometry, material, mesh;
 	init();
-	//animate();
+	animate();
 	function init() 
 		{
 		camera = new THREE.PerspectiveCamera( 75, window.innerWidth /
 		window.innerHeight, 1, 1000 );
 		camera.position.z = 500;
 		scene = new THREE.Scene();
-		//done creating scene
-		var geometry = new THREE.SphereBufferGeometry( 5, 32, 32 );
-		var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-		var sphere = new THREE.Mesh( geometry, material );
-		scene.add( sphere );
-		
+		geometry = new THREE.IcosahedronGeometry( 200, 1 );
+		material = new THREE.MeshBasicMaterial( { color: 0x000000,
+		wireframe: true, wireframeLinewidth: 2 } );
+		mesh = new THREE.Mesh( geometry, material );
+		scene.add( mesh );
 		renderer = new THREE.CanvasRenderer();
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		document.body.appendChild( renderer.domElement );
 		}
-	/*function animate()
+	function animate()
 	{
 		requestAnimationFrame( animate );
-		sphere.rotation.x = Date.now() * 0.00005;
-		sphere.rotation.y = Date.now() * 0.0001;
+		mesh.rotation.x = Date.now() * 0.00005;
+		mesh.rotation.y = Date.now() * 0.0001;
 		renderer.render( scene, camera );
 	}
-	*/
 })
